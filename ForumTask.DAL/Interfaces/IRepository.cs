4414cd1bab0769bd502814152c2c436b95fae929
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-08-02 00:39:44 +0300

    Update repository interface

--- ForumTask.DAL/Interfaces/IRepository.cs
+++ ForumTask.DAL/Interfaces/IRepository.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ForumTask.DAL.Interfaces
 {
@@ -6,57 +9,49 @@ namespace ForumTask.DAL.Interfaces
     {
         /// <summary>
         /// Creates/adds/inserts entity
-        /// <para>
-        /// If <paramref name="ent"/> is null, throws <see cref="ArgumentNullException"/> 
-        /// </para>
         /// </summary>
-        /// <param name="ent">Entity to add</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        void Create(T ent);
+        Task CreateAsync(T entity);
 
         /// <summary>
-        /// Finds entity with providden primary key
-        /// <para>
-        /// If <paramref name="key"/> is null, throws <see cref="ArgumentNullException"/> 
-        /// </para>
+        /// Finds entity by providden predicate
         /// </summary>
-        /// <param name="key">Primary key</param>
         /// <returns>Found entity or null if not found</returns>
-        /// <exception cref="ArgumentNullException" />
-        T Get(params object[] key);
+        Task<T> GetAsync(Func<T, bool> predicate);
+
+        /// <summary>
+        /// Finds all entities by predicate with ordering, offset and take limit
+        /// </summary>
+        /// <param name="predicate">Predicate to use</param>
+        /// <param name="orderBy">Ordering of result</param>
+        /// <param name="offset">Count to skip (if <=0 then not used)</param>
+        /// <param name="take">Count to take (if <=0 then not used)</param>
+        /// <returns>Collection of entities</returns>
+        Task<IEnumerable<T>> GetAllAsync(
+            Func<T, bool> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int offset = 0,
+            int take = 0);
+
+        /// <summary>
+        /// Finds all entities by predicate with ordering, offset and take limit
+        /// </summary>
+        /// <param name="predicate">Predicate to use</param>
+        /// <param name="offset">Count to skip (if <=0 then not used)</param>
+        /// <param name="take">Maximum count (if <=0 then not used)</param>
+        /// <returns>Count of matching entities</returns>
+        Task<long> CountAsync(
+            Func<T, bool> predicate = null,
+            int offset = 0,
+            int take = 0);
 
         /// <summary>
         /// Updates entity
-        /// <para>
-        /// If <paramref name="ent"/> is null, throws <see cref="ArgumentNullException"/> 
-        /// </para>
         /// </summary>
-        /// <param name="ent">Entity to update</param>
-        /// <exception cref="ArgumentNullException" />
-        void Update(T ent);
-
-        /// <summary>
-        /// Deletes entity with providden primary key
-        /// <para>
-        /// If <paramref name="key"/> is null, throw <see cref="ArgumentNullException"/>
-        /// </para>
-        /// <para>
-        /// If entity with such keys isn`t found, throws <see cref="InvalidOperationException"/>
-        /// </para>
-        /// </summary>
-        /// <param name="key">Primary key</param>
-        /// <exception cref="InvalidOperationException"/>
-        /// <exception cref="ArgumentNullException"/>
-        void Delete(params object[] key);
+        Task UpdateAsync(T entity);
 
         /// <summary>
         /// Deletes entity
-        /// <para>
-        /// If <paramref name="ent"/> is null, throws <see cref="ArgumentNullException"/> 
-        /// </para>
         /// </summary>
-        /// <param name="ent">Entity to delete</param>
-        /// <exception cref="ArgumentNullException"/>
-        void Delete(T ent);
+        Task DeleteAsync(T entity);
     }
 }
