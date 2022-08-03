9ef3ee403697c70ee642224d5fe327ac9d39b9a4
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-08-04 01:06:07 +0300

    Change overall folders structure

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ It's project that was created to access laboratory of IT-company.
 
 It's simple forum with classic functions.
 
+    Project was major reworked and cleaned few monthes after presentation.
+
 ## Technology stack
 
-- .NET Core 3.1 (after presentation was updated to .NET 5 than to .NET 6 as experiment)
-- ASP.NET Core 3.1
+- .NET 6 (was core 3.1 at presentation time)
+- ASP.NET Core
 - Entity Framework Core
 - ReactJS for frontend
 - NUnit test framework
---
+++ frontend/src/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "/app.jsx",
+    output: {
+        path: __dirname + '/../publish/',
+        filename: "app.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.css/,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    }
+}
