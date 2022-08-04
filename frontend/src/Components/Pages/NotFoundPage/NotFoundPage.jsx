37a67d8e90e3d44dfd41c7bb34fa41c7b0f3827b
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-08-04 03:25:20 +0300

    Clean NotFound and Profile pages

--- frontend/src/Components/Pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/Components/Pages/NotFoundPage/NotFoundPage.jsx
@@ -2,8 +2,8 @@ import css from "./style.module.css"
 import { Link } from "react-router-dom";
 import React from "react";
 
-export default class NotFoundPage extends React.Component{
-    render(){
+export default class NotFoundPage extends React.Component {
+    render() {
         return (
             <div className={css.page_container}>
                 <h1 className={css.title}>Page Not Found</h1>
